7ea60117324d73e4ed84e1558924ee84c90374c7
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2024-03-28 15:29:28 +0530

    Update urls.py

--- EduCrazePolis/urls.py
+++ EduCrazePolis/urls.py
@@ -18,9 +18,6 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("homePage.urls")),
